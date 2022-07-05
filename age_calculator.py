d317c51e24aa2797c60728d6d79ba5bd2457e192
Nistelq8 <aziz.almarzouq@gmail.com>
2022-07-05 17:01:47 +0300

    Solved task

--- age_calculator.py
+++ age_calculator.py
@@ -1,19 +1,33 @@
 from datetime import date
 
 
-def get_dob():
+def get_dob(year, month, day):
     # write code here
-	...
+    ...
+year=int(input("Enter your year of birth: "))
+month=int(input("Enter your month of birth: "))
+day=int(input("Enter your day of birth: "))
+dob=date(year, month, day)
+# print(dob)
 
 
 def get_age(dob):
     # write code here
-	...
+    ...
+today=date.today()
+age=today-dob
+ageinyears=(age.days//365)
+if ageinyears <= 0:
+    print("You are not born yet")
+else:
+    print("You are", str(ageinyears), "years old")
+
 
 
 def main():
-	# write code here
-	...
+    # write code here
+    ...
+
 
 
 if __name__ == '__main__':
